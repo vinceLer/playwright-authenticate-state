37f7771670462ee0b6d1903bb94261c308d4f49d
vinceLer <91140416+vinceLer@users.noreply.github.com>
2025-10-20 22:26:45 +0200

    Update visual-user.auth.setup.ts

--- tests/setup/visual-user.auth.setup.ts
+++ tests/setup/visual-user.auth.setup.ts
@@ -9,12 +9,12 @@ const USER_FILE = path.join(
 
 setup('[setup] Visual user session', async ({ page }) => {
 
-    // Login code
     await page.goto('https://www.saucedemo.com/');
     await page.locator('[data-test="username"]').fill('visual_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
 
     await page.locator('[data-test="title"]').waitFor();
+    
     await page.context().storageState({ path: USER_FILE });
 });
